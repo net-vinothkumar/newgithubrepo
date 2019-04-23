441c4938e954e5226848511ef4a66158bdd0e2c6
vinothkumar <net.vinothkumar@gmail.com>
2019-04-23 17:51:17 +0200

    new demo repo

---
+++ src/main/java/com/interviewdot/demogitproject/DemogitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.interviewdot.demogitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitprojectApplication.class, args);
+	}
+
+}
